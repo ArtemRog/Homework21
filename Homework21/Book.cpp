0914fa767467fe6bc2f1d0a1ad459115638482ae
Rogozian Artem <artemrogozan@gmail.com>
2023-02-22 00:14:25 +0200

    create cpp file class Book

--- Homework21/Book.cpp
+++ Homework21/Book.cpp
@@ -1,1 +1,29 @@
 #include "Book.h"
+
+Book::Book()
+{
+    rezyme = "no rezyme";
+}
+
+Book::Book(string n, string a, int years, string rezyme):Publication(n,a,years)
+{
+    this->rezyme = rezyme;
+}
+
+void Book::setRezyme(string r)
+{
+    this->rezyme = r;
+}
+
+string Book::getRezyme() const
+{
+    return rezyme;
+}
+
+void Book::show() const
+{
+    cout << "Name:" << name << endl;
+    cout << "Author:" << author << endl;
+    cout << "Years:" << years << endl;
+    cout << "Rezyme:" << rezyme << endl;
+}
