cf8cf300afce9544b39c3a1aa6c964d4003abfb2
Rogozian Artem <artemrogozan@gmail.com>
2023-02-22 00:01:36 +0200

    cpp file class Publication

--- Homework21/Publication.cpp
+++ Homework21/Publication.cpp
@@ -1,1 +1,47 @@
 #include "Publication.h"
+
+Publication::Publication()
+{
+    name = "no name";
+    author = "no author";
+    years = 0;
+}
+
+Publication::Publication(string n, string a, int years)
+{
+        name = n;
+        author = a;
+        this->years = years;
+    
+}
+
+void Publication::setName(string n)
+{
+    name = n;
+}
+
+void Publication::setAuthor(string a)
+{
+    author = a;
+}
+
+void Publication::setYears(int years)
+{
+    
+    this->years = years;
+}
+
+string Publication::getName() const
+{
+    return name;
+}
+
+string Publication::getAuthor() const
+{
+    return author;
+}
+
+int Publication::getYears() const
+{
+    return years;
+}
