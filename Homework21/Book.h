c2bb0a8aedb49171569bd514f96b4d13b626ad33
Rogozian Artem <artemrogozan@gmail.com>
2023-02-22 00:08:41 +0200

    create heder file class Book

---
+++ Homework21/Book.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Publication.h"
+
+class Book:public Publication
+{protected:
+	string rezyme;
+public:
+	Book();
+	Book(string n, string a, int years, string rezyme);
+	void setRezyme(string r);
+	string getRezyme()const;
+	void show()const;
+
+};
+
