99b7b9ebe658a60772710046932cb2abb3272d97
Rogozian Artem <artemrogozan@gmail.com>
2023-02-21 23:56:01 +0200

    create heder file Publication

---
+++ Homework21/Publication.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using std::string;
+using std::endl;
+using std::cout;
+class Publication
+{
+protected:
+	string name;
+	string author;
+	int years;
+public:
+	Publication();
+	Publication(string n, string a, int years);
+	void setName(string n);
+	void setAuthor(string a);
+	void setYears(int years);
+	string getName()const;
+	string getAuthor()const;
+	int getYears()const;
+	virtual void show()const = 0;
+};
+
